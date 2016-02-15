09148788220fc62087941201ef5affe6bea6251a
Kadir Cenk <kadircenk3@hotmail.com>
2016-02-15 22:02:59 +0200

    added local persistence system

--- app/src/main/java/com/kadircenk/drugtracesystem/eczaDolabi.java
+++ app/src/main/java/com/kadircenk/drugtracesystem/eczaDolabi.java
@@ -1,23 +1,28 @@
 package com.kadircenk.drugtracesystem;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class eczaDolabi extends AppCompatActivity implements AdapterView.OnItemClickListener {
     ListView mainListView;
-    ArrayAdapter mArrayAdapter;
-    ArrayList mDrugList = new ArrayList();
+    ArrayAdapter<String> mArrayAdapter;
+    ArrayList<String> mDrugList = new ArrayList<>();
+
+    SharedPreferences sharedPref;
+    SharedPreferences.Editor editor;
+
+    int ilac_sayisi;
 
     String gelenVeri;
 
@@ -27,8 +32,19 @@ public class eczaDolabi extends AppCompatActivity implements AdapterView.OnItemC
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ecza_dolabi);
 
+        sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        ilac_sayisi = sharedPref.getInt("ilac_sayisi", 0);
+
+        Map<String, ?> allEntries = sharedPref.getAll();
+        for (Map.Entry<String, ?> entry : allEntries.entrySet()) {
+            String tmp = entry.getValue().toString();
+            if (tmp.startsWith("ilac+"))
+                mDrugList.add(tmp.substring(5));
+
+        }
+
         mainListView = (ListView) findViewById(R.id.dolap_list);
-        mArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, mDrugList);
+        mArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mDrugList);
         mainListView.setAdapter(mArrayAdapter);
 
         mainListView.setOnItemClickListener(this);
@@ -56,6 +72,15 @@ public class eczaDolabi extends AppCompatActivity implements AdapterView.OnItemC
                 if(data.hasExtra("drugName"))
                 {
                     gelenVeri = data.getStringExtra("drugName");
+                    ilac_sayisi++;
+
+                    sharedPref = eczaDolabi.this.getPreferences(Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPref.edit();
+
+                    editor.putString("ilac" + ilac_sayisi + "+" + gelenVeri, "ilac+" + gelenVeri);
+                    editor.putInt("ilac_sayisi", ilac_sayisi);
+                    editor.commit();
+
                     mDrugList.add(gelenVeri);
                     mArrayAdapter.notifyDataSetChanged();
                 }
