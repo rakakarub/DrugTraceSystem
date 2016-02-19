17b8ff65e95249b1d86555683e3758a80c12b011
Kadir Cenk <kadircenk3@hotmail.com>
2016-02-19 19:11:39 +0200

    added Drug class

---
+++ app/src/main/java/com/kadircenk/drugtracesystem/Drug.java
@@ -0,0 +1,36 @@
+package com.kadircenk.drugtracesystem;
+
+/**
+ * Created by kadircenk on 19.02.2016.
+ */
+public class Drug {
+    private String name;
+    private String skt;
+    private String price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSkt() {
+        return skt;
+    }
+
+    public void setSkt(String skt) {
+        this.skt = skt;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+
+}
